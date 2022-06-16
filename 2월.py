65c9cf7f2ff1e7171b72ead3f372157a6112eb59
JongCheolWon <106072442+JongCheolWon@users.noreply.github.com>
2022-06-16 22:45:10 +0900

    Add files via upload

---
+++ 2월.py
@@ -0,0 +1,50 @@
+from jsonschema import Draft6Validator
+import pandas as pd
+
+file_path1 = '2월.xlsx'
+
+df1 = pd.read_excel(file_path1, sheet_name='스마트조명')#, encoding = 'CP949')
+# 데이터프레임 출력
+print(df1)
+df1.info()
+print('\n')
+
+
+df2 = pd.read_excel(file_path1, sheet_name='온습도감지')#, encoding = 'CP949')
+print(df2)
+df2.info()
+print('\n')
+
+
+df3 = pd.read_excel(file_path1, sheet_name='조도센서')#, encoding = 'CP949')
+print(df3)
+df3.info()
+print('\n')
+
+
+df4 = pd.read_excel(file_path1, sheet_name='스마트큐브')#, encoding = 'CP949')
+print(df4)
+df4.info()
+print('\n')
+
+
+df5 = pd.read_excel(file_path1, sheet_name='미니스위치')#, encoding = 'CP949')
+print(df5)
+df5.info()
+print('\n')
+
+df6 = pd.read_excel(file_path1, sheet_name='열림감지')#, encoding = 'CP949')
+print(df6)
+df6.info()
+print('\n')
+
+
+df7 = pd.read_excel(file_path1, sheet_name='스마트플러그')#, encoding = 'CP949')
+print(df7)
+df7.info()
+print('\n')
+
+
+df8 = pd.read_excel(file_path1, sheet_name='스마트블라인드')#, encoding = 'CP949')
+print(df8)
+df8.info()
